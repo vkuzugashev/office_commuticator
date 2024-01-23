1a22e6e5666bae05ac010d03f6a7a0b9f720d3fa
Vladimir <vkuzugashev@mail.ru>
2024-01-23 21:50:54 +0700

    Refactoring models

--- services/redis_client.py
+++
@@ -1,9 +0,0 @@
-import json
-import redis
-
-r = redis.Redis(host='localhost', port=6379, db=0)
-client_info={'fio': 'Кузугашев Влдаимир Иванович', 'order': '777'}
-r.set('79043744685', json.dumps(client_info))
-cl = json.loads(r.get('79043744685'))
-print(f'client_info={client_info}')
-print(cl)
